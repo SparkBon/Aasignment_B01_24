864659dd15bf988504c5360a38e3b133afa24d4b
Shradha Gilbile <gilbileshradha172001@gmail.com>
2024-04-05 15:52:38 +0530

    Made changes in Exception

--- assig_1_shradha.py
+++ assig_1_shradha.py
@@ -37,54 +37,61 @@ class data_base:
 
       def insert_data(self):
 
-         try:
+         IP="INSERT INTO DATA_TBL  (Id, First_Name, Last_Name, Email, Salary, Gender, Age, City) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+         V=[
+                  (1001, "Shradha", "Gilbile", "shradha@gmail.com", 80000, "female", 22, "Mumbai"),
+                  (1002, "Ditya", "patel", "ditya@gmail.com",45000, "female", 22," Pune" ),
+                  (1003, "Ram", "patil", "ram@gmail.com", 70000, "male", 25, "Mumbai "),
+                  (1004, "Raj", "Jadhav", "raj@gmail.com", 55000, "male", 23, "Kolhapur" ),
+                  (1005, "Yash", "Shinde", "Shinde@gmail.com", 60000, "male", 26, "Nashik" ),
+                  (1006, "Tanmay", "Pandit", "tanmay@gmail.com", 54000, "male", 25, "pune" ),
+                  (1007, "Aadya", "Mehta", "aadya@gmail.com", 60000, "female", 22, "MUmbai" ),
+                  (1008, "vaishnavi", "Pandit", "vaishnavi@gmail.com", 60000, "female", 23,"Mumbai" ),
+                  (1009, "Aaradhya", "mohite", "aaradhya@gmail.com", 45000, "female", 24,"Kolhapur "),
+                  (1010, "Pratik", "Mohite", "pratik@gmail.com", 55000, "male", 22, "pune"),
+                  (1011, "Amol", "Gilbile", "amol@gmail.com", 65000, "male", 20, "Mumbai"),
+                  (1012, "Trupti", "Gilbile", "trupti@gmail.com", 80000, "female", 24,"Mumbai"),
+                  (1013, "Viraj", "Pant", "viraj@gmail.com", 35000, "male", 21,"Sangli"),
+                  (1014, "Rupak", "Jadhav", "rupak@gmail.com", 40000, "male",22 , "Delhi" ),
+                  (1015, "Siddesh", "Desai", "siddesh@gmail.com", 50000, "male", 26, "Kolhapur"),
+                  (1016, "Ira", "Gupta", "ira@gmail.com", 40000, "female", 23, "Gujrat"),
+                  (1017, "Neha", "Joshi", "Neha@gmail.com", 54000, "female", 25, "Pune"),
+                  (1018, "Vaibhav", "Patil", "vaibhav@gmail.com", 50000, "male", 22, "Mumbai"),
+                  (1019, "Nitya", "Mangle", "nitya@gmail.com", 45000, "female", 23, "Kolhapur" ),
+                  (1020, "Aarav", "Mohite", "aarav@gmail.com", 50000, "male", 25,"Pune" )
+                  ]
+         
+         for i in range(1): 
+            try:      
+               if self._Fr_om and self._T_o in range(30000, 80001):
 
-            IP="INSERT INTO DATA_TBL  (Id, First_Name, Last_Name, Email, Salary, Gender, Age, City) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
-            V=[
-                     (1001, "Shradha", "Gilbile", "shradha@gmail.com", 80000, "female", 22, "Mumbai"),
-                     (1002, "Ditya", "patel", "ditya@gmail.com",45000, "female", 22," Pune" ),
-                     (1003, "Ram", "patil", "ram@gmail.com", 70000, "male", 25, "Mumbai "),
-                     (1004, "Raj", "Jadhav", "raj@gmail.com", 55000, "male", 23, "Kolhapur" ),
-                     (1005, "Yash", "Shinde", "Shinde@gmail.com", 60000, "male", 26, "Nashik" ),
-                     (1006, "Tanmay", "Pandit", "tanmay@gmail.com", 54000, "male", 25, "pune" ),
-                     (1007, "Aadya", "Mehta", "aadya@gmail.com", 60000, "female", 22, "MUmbai" ),
-                     (1008, "vaishnavi", "Pandit", "vaishnavi@gmail.com", 60000, "female", 23,"Mumbai" ),
-                     (1009, "Aaradhya", "mohite", "aaradhya@gmail.com", 45000, "female", 24,"Kolhapur "),
-                     (1010, "Pratik", "Mohite", "pratik@gmail.com", 55000, "male", 22, "pune"),
-                     (1011, "Amol", "Gilbile", "amol@gmail.com", 65000, "male", 20, "Mumbai"),
-                     (1012, "Trupti", "Gilbile", "trupti@gmail.com", 80000, "female", 24,"Mumbai"),
-                     (1013, "Viraj", "Pant", "viraj@gmail.com", 35000, "male", 21,"Sangli"),
-                     (1014, "Rupak", "Jadhav", "rupak@gmail.com", 40000, "male",22 , "Delhi" ),
-                     (1015, "Siddesh", "Desai", "siddesh@gmail.com", 50000, "male", 26, "Kolhapur"),
-                     (1016, "Ira", "Gupta", "ira@gmail.com", 40000, "female", 23, "Gujrat"),
-                     (1017, "Neha", "Joshi", "Neha@gmail.com", 54000, "female", 25, "Pune"),
-                     (1018, "Vaibhav", "Patil", "vaibhav@gmail.com", 50000, "male", 22, "Mumbai"),
-                     (1019, "Nitya", "Mangle", "nitya@gmail.com", 45000, "female", 23, "Kolhapur" ),
-                     (1020, "Aarav", "Mohite", "aarav@gmail.com", 50000, "male", 25,"Pune" )
-                     ]
-            
-            Salary=[ records for records in V if records[4] in range(self._Fr_om, self._T_o + 1 )]
+                  Salary=[ records for records in V if records[4] in range(self._Fr_om, self._T_o + 1 )]
+                  
+                  #  Salary=[]
 
-         #  Salary=[]
+                  #  for i in V:
+                  #      if i[4] in range(self._Fr_om, self._T_o + 1): 
+                  #         Salary.append(i)  
 
-         #  for i in V:
-         #      if i[4] in range(self._Fr_om, self._T_o + 1): 
-         #         Salary.append(i)  
 
    
-            for i in Salary:    
-               mycursor.execute(IP, i)
-               mydb.commit()
-               print(mycursor.rowcount,"record inserted")
+               for i in Salary:    
+                  mycursor.execute(IP, i)
+                  mydb.commit()
+                  print(mycursor.rowcount,"record inserted")
+               obj.select()   
 
-         except Exception as e:
-            print("The error is : ", e)    
+            except:
+               print("There is no one between these pay ranges. Enter the range from 30000 to 80000")
+               break     
 
 
       def select(self):
          try:
 
-            mycursor.execute("select * from data_tbl ") 
+            mycursor.execute(f"""select * from data_tbl 
+                             where Salary between {self._Fr_om} and {self._T_o}""")  
+            
             myresult=mycursor.fetchall()
             for i in myresult:
                print(i) 
@@ -103,5 +110,5 @@ obj = data_base(fr_om, t_o)
 mycursor.execute("use Assign_db") 
 # obj.create_table()
 obj.insert_data()
-obj.select()
+
 
